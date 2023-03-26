2327fd035ac3e9db1c8ea7a3a226ff402a449a78
Yoiner Duran <duranyoiner@gmail.com>
2023-03-26 13:33:49 -0500

    agregando comentarios

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
 
+#agrego los modelos al panel de administrador para verlos desde el admin de django
 admin.site.register(Producto)
 admin.site.register(Categoria)
